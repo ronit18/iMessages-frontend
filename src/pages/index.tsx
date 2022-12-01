9808a49ebc4b8645e77a0a27dc1a46893a6dc8b2
ronitgandhi <ronitgandhi@gmail.com>
2022-12-01 18:12:31 +0530

    testing with nextauth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { NextPage } from "next";
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-export default function Home() {
+const Home: NextPage = () => {
+	const { data } = useSession();
+	console.log("data is", data);
 	return (
 		<div>
 			<Head>
@@ -11,7 +15,26 @@ export default function Home() {
 
 			<div>
 				<h1>Hello</h1>
+
+				{data?.user ? (
+					<>
+						{data?.user?.name}
+						<br />
+						<img src={data?.user?.image} alt="me" />
+						<br />
+
+						<button onClick={() => signOut()}>Sign Out</button>
+					</>
+				) : (
+					<>
+						<button onClick={() => signIn("google")}>
+							sign in
+						</button>
+					</>
+				)}
 			</div>
 		</div>
 	);
-}
+};
+
+export default Home;
