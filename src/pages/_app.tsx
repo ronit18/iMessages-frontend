1a8a01232f67d827275b8236087c9088083ba8de
ronitgandhi <ronitgandhi@gmail.com>
2022-12-02 11:44:19 +0530

    added apollo client graphQL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
+import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { client } from "../graphql/apollo-client";
 
 export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
 }: AppProps) {
 	return (
-		<SessionProvider session={session}>
-			<ChakraProvider theme={theme}>
-				<Component {...pageProps} />
-			</ChakraProvider>
-		</SessionProvider>
+		<ApolloProvider client={client}>
+			<SessionProvider session={session}>
+				<ChakraProvider theme={theme}>
+					<Component {...pageProps} />
+				</ChakraProvider>
+			</SessionProvider>
+		</ApolloProvider>
 	);
 }
